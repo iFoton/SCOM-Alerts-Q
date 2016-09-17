ef6cbc82c9a004ae6daf6d510c88094a285fc1c5
iFoton <foton_gem76@list.ru>
2016-09-17 19:17:57 +0300

    Get alert improov

--- getAlert.sql
+++ getAlert.sql
@@ -1,18 +1,61 @@
-SELECT DISTINCT
-	MonitoringObjectId,
-	MonitoringObjectDisplayName,
-	MonitoringObjectName,
-	MonitoringObjectPath,
-	MonitoringObjectFullName,
-	IsMonitorAlert,
-	Priority,
-	Severity,
-	Category,
-	TimeRaised,
-	LastModified,
-	RepeatCount,
-	AlertStringName,
-	AlertStringDescription,
-	AlertParams
-FROM AlertView 
-WHERE ID = 'ALERT_ID'
+DECLARE @Id uniqueidentifier = 'ALERT_ID'
+DECLARE @IsMonitor tinyint = (SELECT DISTINCT IsMonitorAlert FROM AlertView WHERE Id = @id)
+
+IF @IsMonitor = 1
+BEGIN --If Monitor
+	SELECT DISTINCT
+		A.AlertStringName AS 'Alert Name',
+		A.AlertStringDescription AS 'Alert Description',
+		A.TimeRaised,
+		A.RepeatCount,
+		A.Category,
+		A.Severity,
+		A.Priority,
+		M.DisplayName AS 'Monitor Name',
+		M.Description AS 'Monitor Description',
+		MP.DisplayName AS 'Management Pack',
+		A.MonitoringObjectId,
+		A.MonitoringObjectDisplayName,
+		A.MonitoringObjectName,
+		A.MonitoringObjectPath,
+		A.MonitoringObjectFullName,
+		A.AlertParams,
+		KA.KnowledgeContent AS 'Knowledge'
+	FROM AlertView A Join
+	MonitorView M ON
+	A.ProblemId = M.Id JOIN
+	ManagementPackView MP ON
+	M.ManagementPackId = MP.Id JOIN
+	KnowledgeArticle KA ON
+	M.Id = KnowledgeReferenceId
+	WHERE A.Id = @id
+END
+ELSE --If Rule
+BEGIN
+	SELECT DISTINCT
+		A.AlertStringName AS 'Alert Name',
+		A.AlertStringDescription AS 'Alert Description',
+		A.TimeRaised,
+		A.RepeatCount,
+		A.Category,
+		A.Severity,
+		A.Priority,
+		R.DisplayName AS 'Rule Name',
+		R.Description AS 'Rule Description',
+		MP.DisplayName AS 'Management Pack',
+		A.MonitoringObjectId,
+		A.MonitoringObjectDisplayName,
+		A.MonitoringObjectName,
+		A.MonitoringObjectPath,
+		A.MonitoringObjectFullName,
+		A.AlertParams,
+		KA.KnowledgeContent AS 'Knowledge'
+	FROM AlertView A Join
+	RuleView R ON
+	A.MonitoringRuleId = R.Id JOIN
+	ManagementPackView MP ON
+	R.ManagementPackId = MP.Id JOIN
+	KnowledgeArticle KA ON
+	R.Id = KnowledgeReferenceId
+	Where A.Id = @id
+END
